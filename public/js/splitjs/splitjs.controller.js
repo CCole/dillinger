5f24dbd7e25ccbc80481056ec40323ab56e7789f
Beastie <corypcole@gmail.com>
2016-03-23 09:53:41 -0400

    console logged throughout splitjs module to determine if elements passed to the split function were making it through the module.

--- public/js/splitjs/splitjs.controller.js
+++ public/js/splitjs/splitjs.controller.js
@@ -3,23 +3,25 @@ var Split = require('split.js');
 module.exports = 
   angular
   .module('diSplitJs',['diSplitJsDir.directives'])
-  .controller('diSplitJsCtrl', function($rootScope){
-    /* console.log('hit');
+  .controller('diSplitJsCtrl', function($scope){
+    // console.log('hit');
      
      // var editor = angular.element(document.querySelector('#editor')).attr('id'); 
      // var preview = angular.element(document.querySelector('#preview')).attr('id');
       
       var editorElem = document.querySelector('#editor1');
-      //var previewElem = document.querySelector('#preview1');
+      var previewElem = document.querySelector('#preview1');
       
-      console.log(editorElem.parentNode.className); 
-      console.log(editorElem.parentNode.id);
-      console.log(editorElem.id); 
-      console.log(document.querySelector('#test-directive')); 
+     // console.log(editorElem.parentNode.className); 
+     console.log(editorElem.parentNode.id);
+     console.log(editorElem.id); 
+     console.log(previewElem.parentNode.id);
+     console.log(previewElem.id); 
+     // console.log(document.querySelector('#test-directive')); 
       //common parent does not have an id only class
       //g-b g-b--t1of2 split split-editor
       
-      var editor = angular.element(editorElem).attr('id');
+     // var editor = angular.element(editorElem).attr('id');
       //var preview = angular.element(previewElem).attr('id'); 
       
       
@@ -27,12 +29,13 @@ module.exports =
       //var previewElementID = '#'+preview; 
       //alert('#' + editor); 
       
-     /* $scope.editor = editor; 
+     /*$scope.editor = editor; 
       $scope.preview = preview;*/
       
-   /*   Split(['#editor1', '#preview1'],{
+    Split(['#editor1', '#preview1'],{
+         
           sizes: [50,50],
           minSize: 200 
       }); 
-      */
+     
   });
